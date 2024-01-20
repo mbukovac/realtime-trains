de56907f292418e7a8563f5875786f887152b6d4
mbukovac <marko.bukovac@gmail.com>
2024-01-20 23:53:14 +0000

    Use new api functions as an example

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 import credentials
-from icecream import ic
-from realtime_trains.departure_board import DepartureBoard
-from datetime import date, datetime
+from realtime_trains.api import RealtimeTrainsApi, SearchOptions
 
-today = date.today()
-now = datetime.now()
-board = DepartureBoard(credentials.RTT_API_USERNAME, credentials.RTT_API_PASSWORD)
-services = board.get_search_data(from_station="NEM", date=today, time=now)
-
-for service in services:
-	ic(service)
-
+rtt = RealtimeTrainsApi(credentials.RTT_API_USERNAME, credentials.RTT_API_PASSWORD)
+options = SearchOptions(from_station="NEM", to_station="SHP")
+services = rtt.get_search_services(options)
