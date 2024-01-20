b9f984e676009c3e21a2f0f729e0384a5cd907dd
mbukovac <marko.bukovac@gmail.com>
2024-01-20 23:43:47 +0000

    Added fields and validation for some of them

--- realtime_trains/models.py
+++ realtime_trains/models.py
@@ -1,6 +1,6 @@
-from pydantic import BaseModel, Field
-from typing import Dict, Optional
+from typing import Optional
 from datetime import date, datetime
+from pydantic import BaseModel, validator
 
 class Location(BaseModel):
 	name: str
@@ -9,29 +9,62 @@ class Location(BaseModel):
 	system: str
 
 class OriginDestination(BaseModel):
+	@validator("workingTime", pre=True)
+	def parse_workingTime(cls, value):
+		return datetime.strptime(value, "%H%M%S") if value else None
+
+	@validator("publicTime", pre=True)
+	def parse_publicTime(cls, value):
+		return datetime.strptime(value, "%H%M") if value else None
+
 	tiploc: str
 	description: str
-	working_time: Optional[str] = None
-	public_time: Optional[str] = None
+	workingTime: Optional[datetime] = None
+	publicTime: Optional[datetime] = None
 
 class LocationDetail(BaseModel):
+	@validator("gbttBookedArrival", "gbttBookedDeparture", "realtimeArrival", "realtimeDeparture", pre=True)
+	def parse_time(cls, value):
+		return datetime.strptime(value, "%H%M") if value else None
+
 	realtimeActivated: Optional[bool] = None
+	tiploc: str
 	crs: str
 	description: str
-	gbttBookedArrival: Optional[str] = None
-	gbttBookedDeparture: Optional[str] = None
-	isCall: Optional[bool] = None
-	isPublicCall: Optional[bool] = None
-	realtimeArrival: Optional[str] = None
-	realtimeArrivalActual: Optional[bool] = None
-	realtimeDeparture: Optional[str] = None
-	realtimeDepartureActual: Optional[bool] = None
-	platform: Optional[int] = None
-	platformConfirmed: Optional[bool] = None
-	platformChanged: Optional[bool] = None
-	displayAs: Optional[str] = None
+	gbttBookedArrival: Optional[datetime] = None
+	gbttBookedDeparture: Optional[datetime] = None
 	origin: list[OriginDestination]
 	destination: list[OriginDestination]
+	isCall: Optional[bool] = None
+	isPublicCall: Optional[bool] = None
+	realtimeArrival: Optional[datetime] = None
+	realtimeArrivalActual: Optional[bool] = None
+	realtimeArrivalNoReport: Optional[bool] = None
+	realtimeDeparture: Optional[datetime] = None
+	realtimeDepartureActual: Optional[bool] = None
+	realtimeDepartureNoReport: Optional[bool] = None
+	platform: Optional[str] = None
+	platformConfirmed: Optional[bool] = None
+	platformChanged: Optional[bool] = None
+	cancelReasonCode: Optional[str] = None
+	cancelReasonShortText: Optional[str] = None
+	cancelReasonLongText: Optional[str] = None
+	displayAs: Optional[str] = None
+	serviceLocation: Optional[str] = None
+	#wttBookedArrivalDetailed: Optional[str] = None
+	#wttBookedDepartureDetailed: Optional[str] = None
+	#wttBookedPassDetailed: Optional[str] = None
+	#realtimeWttArrivalLatenessDetailed: Optional[str] = None
+	#realtimeGbttArrivalLateness: Optional[str] = None
+	#realtimeWttDepartureLatenessDetailed: Optional[str] = None
+	#realtimeGbttDepartureLateness: Optional[str] = None
+	#realtimePassDetailed: Optional[str] = None
+	#realtimePassActualDetailed: Optional[str] = None
+	#realtimePassNoReportDetailed: Optional[bool] = None
+	#lineDetailed: Optional[str] = None
+	#lineConfirmedDetailed: Optional[bool] = None
+	#pathDetailed: Optional[str] = None
+	#pathConfirmedDetailed: Optional[bool] = None
 
 class Service(BaseModel):
 	locationDetail: LocationDetail
@@ -43,6 +76,10 @@ class Service(BaseModel):
 	atocName: str
 	serviceType: str
 	isPassenger: bool
+	plannedCancel: Optional[bool] = None
+	origin: Optional[list[OriginDestination]] = None
+	destination: Optional[list[OriginDestination]] = None
+	countdownMinutes: Optional[str] = None
 
 class Search(BaseModel):
 	location: Location
