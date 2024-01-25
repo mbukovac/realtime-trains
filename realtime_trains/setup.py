dcd9d2950d1e20379c1954431bdb5aa04f829ef2
mbukovac <marko.bukovac@gmail.com>
2024-01-25 23:31:16 +0000

    Updated version to 0.0.3

--- realtime_trains/setup.py
+++ realtime_trains/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="realtime-trains",
-    version="0.0.2",
+    version="0.0.3",
     description="Unofficial Python library for Realtime Trains API",
     package_dir={"": "realtime_trains"},
 	packages=find_packages(where="realtime_trains"),
